9c7e9bdec1f354ea02ca3f95e6f8993a74ba9699
Jon Distad <jon.distad@gmail.com>
2015-10-31 12:23:28 -0400

    parse graphics control extension

--- Main.hs
+++ Main.hs
@@ -40,11 +40,27 @@ data LScreenDesc = LSD { lsdHasCT    :: !Bool
                        , lsdIsDesc   :: !Bool
                        , lsdCTSize   :: !Int
                        }
-                   
+
+data GfxControlExt = GCE { gceDisposal  :: !Int
+                         , gceUserInput :: !Bool
+                         , gceHasTransp :: !Bool
+                         , gceDelayTime :: !Int
+                         , gceTranspIdx :: !Int
+                         }
+
 type GIFParser = RWST Canvas BS.ByteString Image Get
 
 imageSeparator :: Word8
 imageSeparator = 0x2C
+
+extensionIntroduer :: Word8
+extensionIntroduer = 0x21
+
+graphicControlLabel :: Word8
+graphicControlLabel = 0xF9
+
+endOfGif :: Word8
+endOfGif = 0x3B
 
 
 validateHeader :: Get ()
@@ -62,7 +78,7 @@ parseLSD lsdByte = LSD hasCT colorRes isDesc ctSize
     colorRes = fromIntegral $ ((lsdByte .&. 0x70) `shiftR` 4) + 1
     isDesc = lsdByte .&. 0x08 /= 0
     ctSize = 1 `shift` (fromIntegral (lsdByte .&. 0x07))
-  
+
 toI :: Integral i => i -> Int
 toI = fromIntegral
 
@@ -82,7 +98,7 @@ decodeCanvas = do
     then do colorTable <- getColorTable ctSize
             return $ Canvas w h bgc pxAR (Just colorTable)
     else return $ Canvas w h bgc pxAR Nothing
-    
+
 
 getColorTable :: Int -> Get ColorTable
 getColorTable n = do
@@ -99,6 +115,24 @@ groupsOf n bs = map fst splits
     splits :: [(BS.ByteString, BS.ByteString)]
     splits = tail $ iterate mkSplits (BS.empty, bs)
     mkSplits (_, rem) = BS.splitAt n rem
+
+parseGfxControlExt :: Get GfxControlExt
+parseGfxControlExt = do
+  skip 2 -- Gfx Control Label and Block size (always 0xF9, 0x04)
+  packed <- getWord8
+  delayTimeW <- getWord16le
+  transpIdxW <- getWord8
+  skip 1 -- Block terminator (0x00)
+  let (disp, userInp, hasTransp) = decodeMeta packed
+      delay = toI delayTimeW
+      transpIdx = toI transpIdxW
+  return $ GCE disp userInp hasTransp delay transpIdx
+  where
+    decodeMeta b = (getDisp b, getUserInp b, getTransp b)
+    getDisp b = fromIntegral ((b .&. 0x1C) `shiftR` 2)
+    getUserInp b = b .&. 0x02 /= 0
+    getTransp b = b .&. 0x01 /= 0
+
 
 -- decodeGif :: Get Word8
 -- decodeGif = do
